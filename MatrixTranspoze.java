7504d0df14849870af6b4654f13013cef987dd84
Tuncay Demir <tuncaydemir682@gmail.com>
2022-04-27 00:16:47 +0300

    Create MatrixTranspoze.java

---
+++ MatrixTranspoze.java
@@ -0,0 +1,35 @@
+public class MatrixTranspoze {
+    public static void main(String args[]){
+
+        int list[][] = {{1, 3, 4}, {2, 4, 3}, {3, 4, 5}};
+
+        System.out.println("Matrix : ");
+
+        for(int i = 0; i < 3; i++){
+
+            for(int j = 0; j < 3; j++){
+
+                System.out.print(list[i][j] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+        System.out.println("Transpose : ");
+
+        for(int i = 0; i < 3; i++){
+
+            for(int j = 0; j < 3; j++){
+
+                System.out.print(list[j][i] + " ");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+}
